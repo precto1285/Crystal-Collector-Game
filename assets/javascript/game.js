d97e0e7e76da0d791b6fd29790927a8d7085f219
Philippe Recto <precto1285@gmal.com>
2018-02-20 12:03:53 -0800

    outlining and testing jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+//Use jQuery - - - - 
+
+
+//global variables
+winCount=0;
+loseCount=0;
+compRanNum;
+userChoice=0;
+
+
+//computer generated randon numbers
+    compRanNum = $.randomBetween(19, 120);
+
+//apply function for buttons for user choice
+    
+    //button 1 soul gem onclick:
+    $("#soul").click(function(){
+        $.randomBetween(1, 12);
+    });    
+    
+    //button 1 time gem onclick:
+    $("#time").click(function(){
+        $.randomBetween(1, 12);
+    };
+    
+    //button 1 reality gem onclick:
+    $("#reality").click(function(){
+        $.randomBetween(1, 12);
+    };
+    
+
+    //button 1 power gem onclick:
+    $("#power").click(function(){
+        $.randomBetween(1, 12);
+    });
+    
+    
+    //button 1 mind gem onclick:
+    $("#mind").click(function(){
+        $.randomBetween(1, 12);
+    });
+    
+    
+    //button 1 space gem onclick:
+    $("#space").click(function(){
+        $.randomBetween(1, 12);
+    });    
+    
+//
+
+
+//
+
+
+//
+
+
+//
+
+
+//
+
+
+//
+
+
+//
