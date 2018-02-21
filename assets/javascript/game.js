cbcd2a3dd54f7234cfb1bd7a574811a764e81347
Philippe Recto <precto1285@gmal.com>
2018-02-20 17:04:57 -0800

    Assignment Refresh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,45 +5,47 @@
 winCount=0;
 loseCount=0;
 compRanNum;
+userRanNum;
 userButtonAdd=0;
 
 
 //computer generated randon numbers
-    compRanNum = $.randomBetween(19, 120);
-
+    compRanNum = Math.floor((Math.random() * 12) + 1);
+    document.getElementById("computerPick").innerHTML = compRanNum;
 //apply function for buttons for user choice
     
     //button 1 soul gem onclick:
     $("#soul").click(function(){
-        $.randomBetween(1, 12);
+        var soulNum = Math.floor((Math.random() * 120) + 19);
+
     });    
     
     //button 1 time gem onclick:
     $("#time").click(function(){
-        $.randomBetween(1, 12);
+        var timeNum = Math.floor((Math.random() * 120) + 19);
     };
     
     //button 1 reality gem onclick:
     $("#reality").click(function(){
-        $.randomBetween(1, 12);
+        var realityNum = Math.floor((Math.random() * 120) + 19);
     };
     
 
     //button 1 power gem onclick:
     $("#power").click(function(){
-        $.randomBetween(1, 12);
+        var powerNum = Math.floor((Math.random() * 120) + 19);
     });
     
     
     //button 1 mind gem onclick:
     $("#mind").click(function(){
-        $.randomBetween(1, 12);
+        var mindNum = Math.floor((Math.random() * 120) + 19);
     });
     
     
     //button 1 space gem onclick:
     $("#space").click(function(){
-        $.randomBetween(1, 12);
+        Math.floor((Math.random() * 120) + 19);
     });    
     
 //
