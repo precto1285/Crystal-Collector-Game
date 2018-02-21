64f535be70cb72d63805a01507842d4855a04fb9
Philippe Recto <precto1285@gmal.com>
2018-02-20 18:09:22 -0800

    Assignment Refresh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,62 +2,75 @@
 
 
 //global variables
-winCount=0;
-loseCount=0;
-compRanNum;
-userRanNum;
-userButtonAdd=0;
+winCount = 0;
+loseCount = 0;
+compRanNum = '';
+userRanNum = '';
+userButtonAdd = 0;
 
 
 //computer generated randon numbers
-    compRanNum = Math.floor((Math.random() * 12) + 1);
-    document.getElementById("computerPick").innerHTML = compRanNum;
+
+compRanNum = Math.floor((Math.random() * 12) + 1);
+$("#computerPick").html('compRanNum');
 //apply function for buttons for user choice
-    
-    //button 1 soul gem onclick:
-    $("#soul").click(function(){
-        var soulNum = Math.floor((Math.random() * 120) + 19);
 
-    });    
-    
-    //button 1 time gem onclick:
-    $("#time").click(function(){
-        var timeNum = Math.floor((Math.random() * 120) + 19);
-    };
-    
-    //button 1 reality gem onclick:
-    $("#reality").click(function(){
-        var realityNum = Math.floor((Math.random() * 120) + 19);
-    };
-    
+//button 1 soul gem onclick:
+$("#soul").click(function () {
+    var soulNum = Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    soulNum += userRanNum;
 
-    //button 1 power gem onclick:
-    $("#power").click(function(){
-        var powerNum = Math.floor((Math.random() * 120) + 19);
-    });
-    
-    
-    //button 1 mind gem onclick:
-    $("#mind").click(function(){
-        var mindNum = Math.floor((Math.random() * 120) + 19);
-    });
-    
-    
-    //button 1 space gem onclick:
-    $("#space").click(function(){
-        Math.floor((Math.random() * 120) + 19);
-    });    
-    
+});
+
+//button 2 time gem onclick:
+$("#time").click(function () {
+    var timeNum = Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    timeNum += userRanNum;
+};
+
+//button 3 reality gem onclick:
+$("#reality").click(function () {
+    var realityNum = Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    realityNum += userRanNum;
+};
+
+
+//button 4 power gem onclick:
+$("#power").click(function () {
+    var powerNum = Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    powerNum += userRanNum;
+});
+
+
+//button 5 mind gem onclick:
+$("#mind").click(function () {
+    var mindNum = Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    mindNum += userRanNum;
+});
+
+
+//button 6 space gem onclick:
+$("#space").click(function () {
+    Math.floor((Math.random() * 120) + 19);
+    $(this).hide();
+    spaceNum += userRanNum;
+});
+
 //
-    
-
-//
 
 
-//
+// Update userNumbers with  $(#userNumbers).append();
 
 
-//
+//if users clicks > computer random number, game over. add +1 to loseCount
+
+
+//if users clicks === computer random number, winner! add +1 to winCount
 
 
 //
